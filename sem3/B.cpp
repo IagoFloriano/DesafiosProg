c84f62df421c6479431af6af9b29604cdb27998f
Iago Floriano <iagomfloriano@gmail.com>
2022-06-27 03:07:43 -0300

    Ainda nao funciona

--- sem3/B.cpp
+++ sem3/B.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 using ll = long long;
 
-ll prime = 1000000007;
+ll prime = 1000000007LL;
 
 ll modfat(ll n, ll stop){
   if (n == stop) return 1;
@@ -19,18 +19,28 @@ int main() {
     cin >> temp;
     cols.push_back(make_pair(temp, 0));
   }
+  for (pair<int, int> pipi: cols)
+    cout << pipi.first << " ";
+  cout << endl;
 
   for (ll i = 0; i < c; i++){
     int trash, col;
     cin >> trash >> col;
     cols[col-1].second++;
   }
+  for (pair<int, int> pipi: cols)
+    cout << pipi.first - pipi.second << " ";
+  cout << endl;
 
   vector<ll> fats;
   for (pair<ll,ll> cur: cols){
-    ll temp = modfat(cur.first, cur.second);
-    fats.push_back(temp);
+    ll temp1 = modfat(cur.first, 0);
+    ll temp2 = modfat(cur.second, 0);
+    fats.push_back(temp1/temp2);
   }
+  for (auto f: fats)
+    cout << f << " ";
+  cout << endl;
 
   ll prod = 1;
   for (ll f: fats){

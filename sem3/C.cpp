55e5cc21e66040b401ed37aacffaeb670e143c01
Iago Floriano <iagomfloriano@gmail.com>
2022-06-27 21:12:33 -0300

    C incompleto, B nao funciona

--- sem3/C.cpp
+++ sem3/C.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  ll a, b, n;
+  cin >> a >> b >> n;
+
+  // (b/temp) - ((a-1)/temp)
+
+  vector<ll> primos;
+  for (ll i=0; i < n; i++){
+    ll temp;
+    cin >> temp;
+    primos.push_back(temp);
+  }
+
+  // tamanho do subconjunto
+  for (ll i = 0; i < primos.size(); i++){
+    if (
+  }
 
 }

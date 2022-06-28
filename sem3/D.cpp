e3f543ae08fcdc28326e74a97ce2e78dc6701c4e
IagoFloriano <iagomfloriano@gmail.com>
2022-06-28 16:21:50 -0300

    D funciona

--- sem3/D.cpp
+++ sem3/D.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
 int main() {
+  ll n;
+  cin >> n;
+
+  vector<bool> primos;
+  primos.resize(n, true);
+  primos[0] = primos[1] = false;
+
+  for (ll i = 2; i*i < primos.size(); i++)
+    if (primos[i])
+      for (ll j = i*i; j < primos.size(); j+=i)
+        primos[j] = false;
+
+  ll count = 0;
+  for (ll i = 2; i*2 <= primos.size(); i++){
+    if (primos[i]){
+      if (primos[n-i]) count++;
+    }
+  }
+
+  cout << count << "\n";
 
 }

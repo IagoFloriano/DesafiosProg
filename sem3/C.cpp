db61073b91788d3986b28e45b942d52b5c00877c
IagoFloriano <iagomfloriano@gmail.com>
2022-06-30 16:49:41 -0300

    Consegui tudo finalmente

--- sem3/C.cpp
+++ sem3/C.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 using ll = long long;
+using ull = unsigned ll;
 
 int main() {
   ll a, b, n;
   cin >> a >> b >> n;
-
-  // (b/temp) - ((a-1)/temp)
 
   vector<ll> primos;
   for (ll i=0; i < n; i++){
@@ -16,9 +15,19 @@ int main() {
     primos.push_back(temp);
   }
 
-  // tamanho do subconjunto
-  for (ll i = 0; i < primos.size(); i++){
-    if (
+  bitset<16>curset (1);
+  ll soma = 0;
+  for (curset = 0b1; curset != 0b1 << n ; curset = curset.to_ulong() + 1){
+    ll curprime = 1;
+    for (int i = 0; i < n; i++)
+      if (curset[i] == 1)
+        curprime *= primos[i];
+    ll sign = (2 * (curset.count() % 2)) -1;
+    ll amount =(b/curprime) - ((a-1)/curprime);
+    //cout << "multiplos de " << curprime << " entre " << a << " e " << b << ": " << amount << endl;
+    soma += sign*(amount);
   }
+
+  cout << (b-a+1) - soma << "\n";
 
 }

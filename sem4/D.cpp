4886554b89cc37dc3a105bf2b4386303b532e515
Iago Floriano <iagomfloriano@gmail.com>
2022-07-01 16:40:06 -0300

    D nao funciona

--- sem4/D.cpp
+++ sem4/D.cpp
@@ -1,6 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
 int main() {
+  ll t;
+  cin >> t;
+  for(ll i=1; i<=t; i++){
+    ll quant = 0;
+    string corredor;
+    cin >> corredor;
+    
+    for (ll j=1;j<corredor.length();j++){
+      if (corredor[j-1] != '#'){
+        quant++;
+        j+=2;
+      }
+    }
+    
+    cout << "Caso " << i << ": " << quant << "\n";
+  }
 
 }

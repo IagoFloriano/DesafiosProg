1918d380f952474a5fe03d6c1cc36f77e9d0163c
IagoFloriano <iagomfloriano@gmail.com>
2022-07-01 15:00:20 -0300

    B ta errada

--- sem4/B.cpp
+++ sem4/B.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
 int main() {
+  vector<ll> moedas = {1,5,10,25,50};
+  ll valor;
 
+  while (cin >> valor){
+    ll formas = 1;
+    for (ll i = 1; i < 5; i++){
+      if (valor < moedas[i])
+        break;
+      
+      formas += valor / moedas[i];
+    }
+    cout << formas << "\n";
+  }
 }

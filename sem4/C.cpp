9f778561c7eaf4c2aba6e340bc77c9d8c18392e9
Iago Floriano <iagomfloriano@gmail.com>
2022-07-01 15:43:56 -0300

    C funcionando

--- sem4/C.cpp
+++ sem4/C.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
 int main() {
+  ll n, soma, temp;
+  cin >> n;
+  soma = 0;
+  for (ll i=0; i<n;i++){
+    cin >> temp;
+    if(temp > 10)
+      soma += temp-10;
+  }
+  cout << soma << "\n";
 
 }

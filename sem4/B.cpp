7924f83ce5e6d828247df5231bc0ea4b2ba1ef08
Iago Floriano <iagomfloriano@gmail.com>
2022-07-06 14:49:45 -0300

    Ideia em b que nao funciona

--- sem4/B.cpp
+++ sem4/B.cpp
@@ -2,31 +2,25 @@
 using namespace std;
 using ll=long long;
 
-vector<ll> moedas = {1,5,10,25,50};
-
-vector<vector<ll>> lookup;
-
-ll formas(ll n, ll lcoin){
-  ll quant = 0;
-  if(lcoin < 0 || n < 0) return 0;
-  if(lookup[lcoin][n] != 0) return lookup[lcoin][n];
-  if(n == 0){
-    lookup[lcoin][0] = 1;
-    return 1;
-  }
-  quant += formas(n, lcoin-1);
-  quant += formas(n - moedas[lcoin], lcoin);
-  return quant;
-}
 
 int main() {
   ll valor;
-  for(int i=0; i<5; i++){
-    vector<ll> temp (3 * (ll)1e4);
-    lookup.push_back(temp);
-  }
+  vector<ll> moedas = {1,5,10,25,50};
+  vector<ll> formas = {1,2,4,13,50};
 
   while (cin >> valor){
-    cout << formas(valor,4) << "\n";
+    ll quant = 0;
+    ll lcoin = 4;
+    while(valor > 0){
+      if (valor < moedas[lcoin]){
+        lcoin--;
+      }
+      else {
+        quant += formas[lcoin];
+        valor -= moedas[lcoin];
+      }
+      cout << "Valor: " << valor << "\tQuant: " << quant << endl;
+    }
+    cout << quant << "\n";
   }
 }

5441550fc2b02313e1a12de7e5f79d27490a90c4
Iago Floriano <iagomfloriano@gmail.com>
2022-07-06 14:20:39 -0300

    B TL

--- sem4/B.cpp
+++ sem4/B.cpp
@@ -2,18 +2,31 @@
 using namespace std;
 using ll=long long;
 
+vector<ll> moedas = {1,5,10,25,50};
+
+vector<vector<ll>> lookup;
+
+ll formas(ll n, ll lcoin){
+  ll quant = 0;
+  if(lcoin < 0 || n < 0) return 0;
+  if(lookup[lcoin][n] != 0) return lookup[lcoin][n];
+  if(n == 0){
+    lookup[lcoin][0] = 1;
+    return 1;
+  }
+  quant += formas(n, lcoin-1);
+  quant += formas(n - moedas[lcoin], lcoin);
+  return quant;
+}
+
 int main() {
-  vector<ll> moedas = {1,5,10,25,50};
   ll valor;
+  for(int i=0; i<5; i++){
+    vector<ll> temp (3 * (ll)1e4);
+    lookup.push_back(temp);
+  }
 
   while (cin >> valor){
-    ll formas = 1;
-    for (ll i = 1; i < 5; i++){
-      if (valor < moedas[i])
-        break;
-      
-      formas += valor / moedas[i];
-    }
-    cout << formas << "\n";
+    cout << formas(valor,4) << "\n";
   }
 }

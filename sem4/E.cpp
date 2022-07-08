1797da9501c09727f6f4e438341c6a09b3d0c434
IagoFloriano <iagomfloriano@gmail.com>
2022-07-08 15:31:01 -0300

    E foi

--- sem4/E.cpp
+++ sem4/E.cpp
@@ -1,48 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll=long long;
+using ll=int;
 
+vector<vector<ll>> dp;
 vector<ll> quadrados;
-vector<ll> csomas;
 ll n, k, c;
-
-ll minlapis(vector<ll>& pos, ll curr, ll currsum){
-  if(curr >= pos.size()) return currsum;
-  ll best = LLONG_MAX;
-  ll i = curr == 0 ? 0: pos[curr-1]+c;
-  for(; i < n-c+1; i++){
-    if( (pos.size()-curr)*c > csomas.size()-i+1){
-      break;
-    }
-    pos[curr] = i;
-    ll temp = minlapis(pos, curr+1, currsum + csomas[i]);
-    best = min(temp,best);
-  }
-  return best;
-}
 
 int main() {
   cin >> n >> k >> c;
-  vector<ll> poslapis(k, -c);
-  ll total=0;
+  quadrados = vector<ll>(n+1);
+  dp = vector<vector<ll>>(k+1,vector<ll>(n+1,-1000000000));
 
-  for(ll i = 0; i < c; i++){
-    ll temp;
-    cin >> temp;
-    total+=temp;
-    quadrados.push_back(temp);
+  dp[0][0] = 0;
+
+  for(ll i = 1; i <=n; i++){
+    cin >> quadrados[i];
+    dp[0][i] = dp[0][i-1]+quadrados[i];
   }
 
-  csomas.push_back(total);
-
-  for(ll i = c; i < n; i++){
-    ll temp;
-    cin >> temp;
-    total+=temp;
-    quadrados.push_back(temp);
-    csomas.push_back(csomas[i-c] -quadrados[i-c] + temp);
+  for(ll i = 1; i <= k; i++){
+    for(ll j = i*c; j <= n; j++){
+      dp[i][j] = max( dp[i-1][j-c], dp[i][j-1]+quadrados[j] );
+    }
   }
 
-  total -= minlapis(poslapis, 0, 0);
-  cout << total << endl;
+
+  cout << dp[k][n] << "\n";
+
 }

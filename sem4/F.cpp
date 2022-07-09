68442e7604a6a91335ea13e65e50a0e4ecfe478c
Iago Floriano <iagomfloriano@gmail.com>
2022-07-08 22:19:28 -0300

    Desisti da vodka

--- sem4/F.cpp
+++ sem4/F.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define INF 1000000000
+
+int N, I, M, P;
+vector<int> C, V;
+vector<vector<int>>dp; //dp[i][j] menor custo de vodka para i anos com destilador idade j
 
 int main() {
+  cin >> N >> I >> M >> P;
+  C = vector<int> (M);
+  V = vector<int> (M+1);
+  dp = vector<vector<int>>(N+1,vector<int>(M+1,INF));
+
+  for(int i=0; i < M; i++)
+    cin >> C[i];
+  for(int i=1; i <= M; i++)
+    cin >> V[i];
+
+  for(int i=0; i<=M; i++)
+    dp[0][i] = 0;
+
+  for(int i=1; i<=N; i++){
+    for(int j=1; j<=M; j++){
+    }
+  }
+
+  cout << dp[N][I] << endl;
 
 }

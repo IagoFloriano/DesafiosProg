27289657e086b4ab68d62595b7b1e5890b542835
IagoFloriano <iagomfloriano@gmail.com>
2022-07-12 16:33:39 -0300

    E 0ms

--- sem5/E.cpp
+++ sem5/E.cpp
@@ -3,5 +3,17 @@ using namespace std;
 using ll=long long;
 
 int main() {
+  ll a, b, c;
+  cin >> a >> b >> c;
+  //r = -b +- sqrt(b² - 4ac)
+  ll totest = (b * b) - (4 * a * c);
+  double lixo;
+  double resultado;
+  resultado = sqrt(totest);
+  if(modf(resultado, &lixo) == 0)
+    cout << "Y";
+  else
+    cout << "N";
+  cout << "\n";
 
 }

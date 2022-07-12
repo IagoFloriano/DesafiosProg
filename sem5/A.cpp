ec948b7c96a3ff13c903d32a265072a2434a9e4d
IagoFloriano <iagomfloriano@gmail.com>
2022-07-12 16:39:46 -0300

    Começo da A

--- sem5/A.cpp
+++ sem5/A.cpp
@@ -3,5 +3,16 @@ using namespace std;
 using ll=long long;
 
 int main() {
+  ll n, h;
+  cin >> n >> h;
+
+  vector<pair<ll,ll>>p(n);
+  for(auto& i:p)
+    cin >> i.first;
+
+  for(ll i = h; i < n; i++){
+    cout << p[i].first << "\t";
+  }
+  cout << endl;
 
 }

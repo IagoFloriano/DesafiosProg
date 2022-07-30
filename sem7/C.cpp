2d01c84960c50c15a58d08ca6be9785878e684eb
Iago Floriano <iagomfloriano@gmail.com>
2022-07-29 23:04:02 -0300

    C foi

--- sem7/C.cpp
+++ sem7/C.cpp
@@ -3,5 +3,12 @@ using namespace std;
 using ll=long long;
 
 int main() {
+  unsigned long long h, n, v, result;
+  cin >> h >> n >> v;
+  result = v/h;
+  result = (unsigned long long)ceil(result/(double)n);
+  if(n == 1)
+    result = v/h;
 
+  cout << result << "\n";
 }

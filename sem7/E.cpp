ed3479348389fe2405e085ff47cf12cb4c86b5fb
Iago Floriano <iagomfloriano@gmail.com>
2022-08-02 18:51:56 -0300

    arruma E

--- sem7/E.cpp
+++ sem7/E.cpp
@@ -3,19 +3,5 @@ using namespace std;
 using ll=long long;
 
 int main() {
-  ll T;
-  cin >> T;
-  while(T--){
-    ll Ax, Ay, Bx, By;
-    cin >> Ax >> Ay >> Bx >> By;
-
-    if(Ax == Bx)
-      cout << abs(Ay - By) + 1;
-    else if(By == Ay)
-      cout << abs(Ax - Bx) + 1;
-    else
-      cout << __gcd(abs(Ay - By), abs(Ax - Bx)) + 1;
-    cout << "\n";
-  }
 
 }

7292d6368b4244f328cbdca81524e141e564ddd4
Iago Floriano <iagomfloriano@gmail.com>
2022-08-03 13:58:11 -0300

    D foi

--- sem8/D.cpp
+++ sem8/D.cpp
@@ -3,5 +3,17 @@ using namespace std;
 using ll=long long;
 
 int main() {
+  ll H, M, S;
+  cin >> H >> M >> S;
+  ll quant = __gcd(H, __gcd(M,S));
+  printf("%d\n", quant);
+  ll h, m, s;
+  h = m = s = 0;
+  for(ll i = 0; i < quant; i++){
+    printf("%d %d %d\n",h,m,s);
+    h += H/quant;
+    m += M/quant;
+    s += S/quant;
+  }
 
 }

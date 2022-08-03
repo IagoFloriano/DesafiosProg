cff6b36a4de724f3b64cfe9b1c569f22cbd2ec2f
Iago Floriano <iagomfloriano@gmail.com>
2022-08-03 14:23:53 -0300

    Começo F

--- sem8/F.cpp
+++ sem8/F.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 using ll=long long;
 
+vector<int> N;
+
+int rec(int n){
+  if(N[n] != -1) return N[n];
+  return rec(n-2) % rec(n-1);
+}
+
 int main() {
+  ll T;
+  cin >> T;
+  while(T--){
+    ll n, d;
+    cin >> n >> d;
+    N = vector<int>(,-1);
+  }
 
 }

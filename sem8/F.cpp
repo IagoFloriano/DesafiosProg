8371411724e745cced9533f432ae1211248eb338
Iago Floriano <iagomfloriano@gmail.com>
2022-08-04 17:44:53 -0300

    F foi

--- sem8/F.cpp
+++ sem8/F.cpp
@@ -6,8 +6,25 @@ int main() {
   ll T;
   cin >> T;
   while(T--){
-    ll n, d;
+    ll n, d, temp, intero;
     cin >> n >> d;
+    intero = n/d;
+    n -= intero*d;
+    temp = d;
+    d = n;
+    n = temp;
+    printf("[%d", intero);
+    char c = ';';
+    while(d != 0){
+      intero = n/d;
+      n -= intero*d;
+      temp = d;
+      d = n;
+      n = temp;
+      printf("%c%d",c,intero);
+      c = ',';
+    }
+    printf("]\n");
   }
 
 }

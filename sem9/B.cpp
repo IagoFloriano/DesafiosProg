69e58ee2b7627ee5a6952b3fb89933fab82ef8b7
Iago Floriano <iagomfloriano@gmail.com>
2022-08-10 14:10:36 -0300

    B foi

--- sem9/B.cpp
+++ sem9/B.cpp
@@ -9,15 +9,16 @@ vector<int> ammount;
 int counting;
 vector<vertice> g;
 
-void dfs(int u){
-  explored[u] = 1;
+void dfs(int u, int exp){
+  explored[u] = exp;
   edges e = g[u].second;
   for(int n: e){
-    if(explored[n]) continue ;
+    if(explored[n] == exp) continue ;
 
     if(ammount[n] == 1) counting++;
+    if(ammount[n] == 2) continue;
     ammount[n]++;
-    dfs(n);
+    dfs(n,exp);
   }
 }
 
@@ -36,18 +37,18 @@ int main() {
     g[u].second.push_back(v);
   }
 
-  // printf("GRAFO:\n");
-  // for(int i = 0; i < n; i++){
-  //   printf("%d(%d):",i,g[i].first);
-  //   for(auto j: g[i].second)
-  //     printf("\t%d",j);
-  //   printf("\n");
-  // }
+  //printf("GRAFO:\n");
+  //for(int i = 0; i < n; i++){
+  //  printf("%d(%d):",i,g[i].first);
+  //  for(auto j: g[i].second)
+  //    printf("\t%d",j);
+  //  printf("\n");
+  //}
 
+  explored = vector<int>(n,0);
   for(int i = 0; i < n; i++){
     if (g[i].first) continue;
-    explored = vector<int>(n,0);
-    dfs(i);
+    dfs(i,i+1);
   }
 
   printf("%d\n",counting);
